203a2f4a950df005e811b0489df6a60481a82112
Jesse <szx961102@gmail.com>
2020-08-01 13:35:53 +0800

    finish

--- node/app.js
+++ node/app.js
@@ -1,19 +1,29 @@
-const { say } = require('../pkg/ssvm_nodejs_starter_lib.js');
+const {
+    base64Encode,
+    base64Decode,
+} = require('../pkg/ssvm_nodejs_starter_lib.js')
 
-const http = require('http');
-const url = require('url');
-const hostname = '0.0.0.0';
-const port = 3000;
+const http = require('http')
+const url = require('url')
+const hostname = '0.0.0.0'
+const port = 3000
 
 const server = http.createServer((req, res) => {
-  const queryObject = url.parse(req.url,true).query;
-  if (!queryObject['name']) {
-    res.end(`Please use command curl http://${hostname}:${port}/?name=MyName \n`);
-  } else {
-    res.end(say(queryObject['name']) + '\n');
-  }
-});
+    const queryObject = url.parse(req.url, true).query
+    if (queryObject['encodeStr']) {
+        res.end(base64Encode(queryObject['encodeStr']) + '\n')
+    } else if (queryObject['decodeStr']) {
+        console.log(queryObject['decodeStr'])
+        res.end(base64Decode(queryObject['decodeStr']) + '\n')
+    } else {
+        res.end(
+            `Please use command curl http://${hostname}:${port}/?encodeStr=string
+or
+http://${hostname}:${port}/?decodeStr=string \n`
+        )
+    }
+})
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+    console.log(`Server running at http://${hostname}:${port}/`)
+})
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,27 @@
+use base64::{decode, encode};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn say(s: &str) -> String {
-  println!("The Rust function say() received {}", s);
-  let r = String::from("hello ");
-  return r + s;
+pub fn base64Encode(s: &str) -> String {
+    encode(s)
+}
+
+#[wasm_bindgen]
+pub fn base64Decode(s: &str) -> String {
+    // String::from_utf8(decode(s).unwrap()).unwrap()
+    String::from_utf8(decode(s).unwrap()).unwrap()
+}
+
+#[test]
+fn test() {
+    let s = "jesse";
+    let a = "amVzc2U=";
+
+    // dbg!(base64Encode(s));
+    // dbg!(base64Decode(a));
+
+    let encode = base64Encode(s);
+    dbg!(&encode);
+    let decode = base64Decode(encode.as_str());
+    dbg!(decode);
 }
